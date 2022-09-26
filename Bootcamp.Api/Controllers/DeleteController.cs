bfdab022549f6668abea7106a1042981dbdd3c14
DiegoZH <diegozev@hotmail.com>
2022-09-26 11:41:42 -0500

    Reto 1 Terminado

---
+++ Bootcamp.Api/Controllers/DeleteController.cs
@@ -0,0 +1,28 @@
+﻿using Bootcamp.Model;
+using Bootcamp.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteController : ControllerBase
+    {
+        private readonly IPersonRepository _personRepository;
+
+        public DeleteController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public async Task<ActionResult> Delete([FromBody] Person person)
+        {
+            var result = await _personRepository.Delete(person);
+            return Ok(result);
+        }
+
+
+    }
+}
